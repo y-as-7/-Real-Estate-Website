4a24fd3520af6f70f2832d7097dfaeaa24744b97
youssef askar <youssefaskar111@outlook.com>
2023-04-17 17:04:00 +0200

    slider section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Header } from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import Companies from "./components/Companies/Companies";
+import Residencies from "./components/Residencies/Residencies";
 import './App.css'
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Hero />
     </div>
       <Companies />
+      <Residencies />
       </div>
   );
 }
